5e05c301612f5f6b556ff7d1f5a6323d847ce13d
Vitor Hugo <vitorhbts@gmail.com>
2022-05-25 18:45:28 -0300

    first commit

---
+++ src/routes/stack.routes.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { Splash } from '../lotties/Splash';
+//import Login from '../screens/Login';
+import AuthRoutes from './tab.routes';
+//import Usuarios from '../screens/Usuarios';
+//import NovoUsuario from '../screens/NovoUsuario';
